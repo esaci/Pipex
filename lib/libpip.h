59aa8504dd16b0a60b1f36ac3dc659f96e0b80c8
Elias SACI <esaci@e1r6p3.42.fr>
2021-07-12 16:16:16 +0200

    auto

---
+++ lib/libpip.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libpip.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esaci <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/12 14:38:47 by esaci             #+#    #+#             */
+/*   Updated: 2021/07/12 14:38:49 by esaci            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef LIBPIP_H
+# define LIBPIP_H
+
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+#endif
